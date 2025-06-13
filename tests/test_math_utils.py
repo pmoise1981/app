4d6bd8a39ff276e4705f312052db565f0e3eb250
Pierre Moise <moisepier@hotmail.com>
2025-06-12 21:33:22 -0700

    fixed error in test file

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -4,5 +4,5 @@ def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
-    assert subtract(10, 4) == 8  # ← This is intentionally broken
+    assert subtract(10, 4) == 6  # ← This is intentionally broken
 
